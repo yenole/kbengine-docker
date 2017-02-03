81e971b3c282844361e14d33af5f6469c4873a85
Yenole <Netxy@vip.qq.com>
2017-02-03 20:29:33 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,20 @@ MAINTAINER yenole <Netxy@vip.qq.com>
 
 # 更新并安装环境
 RUN apt-get update
-RUN apt-get install -y libmysqlclient-dev libssl-dev python telnet
+RUN apt-get install -y libmysqlclient20 telnet
 
 # 环境变量
+ENV KBE_IDX 0
+ENV KBE_NUM 10
 ENV KBE_ROOT /var/lib/kbengine
 ENV KBE_RES_PATH $KBE_ROOT/kbe/res/:$KBE_ROOT/assets:$KBE_ROOT/assets/res/:$KBE_ROOT/assets/scripts/
 ENV KBE_BIN_PATH $KBE_ROOT/kbe/bin/server/
 
 # 服务器脚本和资源挂载目录
-VOLUME ["/var/lib/kbengine/assets","/var/lib/kbengine/kbe"]
+VOLUME ["/var/lib/kbengine/"]
+
+# 设置工作目录
+WORKDIR /var/lib/kbengine/assets
 
 # 映射端口
 EXPOSE 20013
--- start.sh
+++ start.sh
@@ -1,18 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 echo KBE_ROOT = \"${KBE_ROOT}\"
 echo KBE_RES_PATH = \"${KBE_RES_PATH}\"
 echo KBE_BIN_PATH = \"${KBE_BIN_PATH}\"
 
-cd /var/lib/kbengine/assets
-$KBE_BIN_PATH/machine --cid=2129652375332859700 --gus=1&
-$KBE_BIN_PATH/logger --cid=1129653375331859700 --gus=2&
-$KBE_BIN_PATH/interfaces --cid=1129652375332859700 --gus=3&
-$KBE_BIN_PATH/dbmgr --cid=3129652375332859700 --gus=4&
-$KBE_BIN_PATH/baseappmgr --cid=4129652375332859700 --gus=5&
-$KBE_BIN_PATH/cellappmgr --cid=5129652375332859700 --gus=6&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859700 --gus=7&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859700 --gus=8&
-$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --gus=9&
+$KBE_BIN_PATH/logger 		--cid=1129653375331859700 --gus=1&
+$KBE_BIN_PATH/interfaces 	--cid=1129652375332859700 --gus=2&
+$KBE_BIN_PATH/machine 		--cid=2129652375332859700 --gus=3&
+$KBE_BIN_PATH/dbmgr 		--cid=3129652375332859700 --gus=4&
+$KBE_BIN_PATH/baseappmgr 	--cid=4129652375332859700 --gus=5&
+$KBE_BIN_PATH/cellappmgr 	--cid=5129652375332859700 --gus=6&
+$KBE_BIN_PATH/baseapp 		--cid=6129652375332859700 --gus=7&
+$KBE_BIN_PATH/cellapp 		--cid=7129652375332859700 --gus=8&
+$KBE_BIN_PATH/loginapp 		--cid=8129652375332859700 --gus=9&
 
 bcid=6129652375332859700
 ccid=7129652375332859700
